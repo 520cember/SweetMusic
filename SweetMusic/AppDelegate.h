df91f5e87586b75ff4e9e311ee726b689c9ffe49
520cember <1029278668@qq.com>
2016-02-23 22:30:36 +0800

    Initial Commit

---
+++ SweetMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SweetMusic
+//
+//  Created by 董兴斌 on 16/2/23.
+//  Copyright © 2016年 董兴斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SweetMusic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SweetMusic
+//
+//  Created by 董兴斌 on 16/2/23.
+//  Copyright © 2016年 董兴斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
